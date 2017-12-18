d5a81f14ca736a083ce47351c17d26896bb86585
Kalalihapulla <raasjer@gmail.com>
2017-12-18 21:32:18 +0200

    Unix combination project.

---
+++ lab5/src/lab5/Graph.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jebu
+ */
+public class Graph {
+
+    private ArrayList<LinkedList<Integer>> graph = new ArrayList<LinkedList<Integer>>();
+    private int nodes;
+    public boolean reach = false;
+
+    public void readGraph(File file) {
+        
+        try {
+            Scanner sc = new Scanner(file);
+            int count = 0;
+            while (sc.hasNextLine()) {
+
+                this.graph.add(new LinkedList<Integer>());
+                String i = sc.nextLine();
+                String[] parts = i.split(" ");
+                for (int a = 0; a < parts.length; a++) {
+                    this.graph.get(count).add(Integer.parseInt(parts[a]));
+                }
+                count = count + 1;
+            }
+            sc.close();
+            for (int c = 0; c < count; c++) {
+                Collections.reverse(this.graph.get(c));
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void printGraph() {
+
+        for (LinkedList<Integer> a : this.graph) {
+            LinkedList<Integer> tempList = new LinkedList<Integer>();
+            for (int c = 0; c < a.size() - 1; c++) {
+                tempList.add(a.get(c));
+            }
+            System.out.println(a.get(a.size() - 1).toString() + ": " + tempList.toString());
+        }
+    }
+
+    public int nodes() {
+        
+        this.nodes = this.graph.size();
+        return this.nodes;
+    }
+
+    void dfs(int start, boolean visited[], int pred[], int searched) {
+         if (start == searched) {
+                this.reach= true;
+                return;
+            }
+        
+        searched = searched;
+        visited[start] = true;
+        for (Integer e : this.graph.get(start)) {
+            if (e != null && visited[e] == false) {
+                pred[e] = start;
+                dfs(e, visited, pred, searched);
+            }
+        }
+
+    }
+}
---
+++ lab5/src/lab5/Lab5.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Jebu
+ */
+public class Lab5 {
+
+    /* find a maze solution */
+    private static int mazeSolution(int from, int to, int pred[], int steps[]) {
+
+        int i, n, node;
+
+        // first count how many edges between the end and the start
+        node = to;
+        n = 1;
+
+        while ((node = pred[node]) != from) {
+
+            n++;
+
+        }
+
+        // then reverse pred[] array to steps[] array
+        node = to;
+        i = n;
+
+        while (i >= 0) {
+            steps[i--] = node;
+            node = pred[node];
+        }
+
+        // include also the end vertex
+        return (n + 1);
+    }
+
+    private final static String FILE = "C:/Users/Jebu/Desktop/lab5/maze.grh";
+    private final static String FILE2 = "C:/Users/Jebu/Desktop/lab5/maze2.grh";
+    private final static String FILE3 = "C:/Users/Jebu/Desktop/lab5/maze3.grh";
+    private final static String FILE4 = "C:/Users/Jebu/Desktop/lab5/maze4.grh";
+
+    static String[] mazeArray = {FILE, FILE2, FILE3, FILE4};
+
+    private final static int FROM = 0;
+    private final static int TO = 15;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        
+        int count = 4;
+        try {
+            String arg1 = args[0];
+
+            count = Integer.parseInt(arg1);
+            if (count > 4) {
+                count = 4;
+
+                System.out.println("Max is 4");
+            }
+        } catch (Exception e) {
+        }
+        
+        String FILENAME = "C:/Users/Jebu/Desktop/lab5/output.txt";
+        
+       	BufferedWriter bw = null;
+	FileWriter fw = null;
+ 
+	fw = new FileWriter(FILENAME);
+        bw = new BufferedWriter(fw);
+
+        String solution;
+       
+        for (int i = 0; i < count; i++) {
+            Graph g = new Graph();
+
+            // read the graph. and do the depth-first search
+            System.out.println("Graph Adjacent list");
+            g.readGraph(new File(mazeArray[i]));
+            g.printGraph();
+
+            boolean visited[] = new boolean[g.nodes()];
+            int pred[] = new int[g.nodes()];
+            g.dfs(FROM, visited, pred, g.nodes() - 1);
+
+            System.out.println("Pred array: " + Arrays.toString(pred));
+            System.out.println("Visited array: " + Arrays.toString(visited).replace("true", "1").replace("false", "0"));
+
+            // then check if there is a solution by looking from the backwards to the start
+            int steps[] = new int[g.nodes()];
+
+            System.out.println("\nMaze solution from " + FROM + " to " + TO);
+
+            int n = mazeSolution(FROM, TO, pred, steps);
+
+           if (g.reach) {
+
+                for (int u = 0; u < n; u++) {       
+                   
+                    solution = steps[u] + " ";
+                    
+                    bw.write(solution);
+                                    
+                }
+
+            } else {
+                solution = "No solution for given maze";
+  
+                bw.write(solution);
+            }
+            bw.write(System.getProperty("line.separator"));
+           
+        }
+        
+        
+        bw.close();
+        fw.close();
+    }
+
+}
